317b470d15eb3c746fa6e8cdea9aeeffebd54922
Federico <59148362+FGP310592@users.noreply.github.com>
2024-04-23 17:09:47 +0200

    Delete Refrigerant_hT.m

--- Refrigerant_hT.m
+++
@@ -1,9 +0,0 @@
-function [T] = Refrigerant_hT(TSat,TSatVar,cpL,cpG,hLSat,hGSat,h)
-if(h > hGSat)
-    T = (TSat+TSatVar/2)+(h-hGSat)/cpG;
-elseif(h < hLSat)
-    T = (TSat-TSatVar/2)+(h-hLSat)/cpL;
-else
-    T = (TSat+TSatVar/2)+(h-hLSat)/(hGSat-hLSat)*TSatVar;
-end
-end
